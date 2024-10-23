8157dbd3c5f8ee7a872ef8c059eb98d4b5c552f3
Davy de Bruin <davydb024@gmail.com>
2024-10-23 23:03:27 +0200

    Create HANStack.java

---
+++ startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -0,0 +1,32 @@
+package nl.han.ica.datastructures;
+
+import java.util.ArrayList;
+
+public class HANStack<T> implements IHANStack<T> {
+    private ArrayList<T> stack;
+
+    public HANStack() {
+        this.stack = new ArrayList<>();
+    }
+
+    @Override
+    public void push(T value) {
+        stack.add(value);
+    }
+
+    @Override
+    public T pop() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public T peek() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.get(stack.size() - 1);
+    }
+}
