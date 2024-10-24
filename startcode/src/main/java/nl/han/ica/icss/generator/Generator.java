a620d88bff5c5328f249c1fca69a33cff57e293e
Davy de Bruin <davydb024@gmail.com>
2024-10-24 20:35:25 +0200

    Generator Lvl 0

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,33 +1,85 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import nl.han.ica.icss.ast.literals.ScalarLiteral;
 
 public class Generator {
 
-	public String generate(AST ast) {
-        return generateStylesheet((Stylesheet) ast.root);
-	}
+    public String generate(AST ast) {
+        return generateStylesheet(ast.root);
+    }
 
-	private String generateStylesheet(Stylesheet root) {
-		return generateStylerule((Stylerule)root.getChildren().get(0));
-	}
+    private String generateStylesheet(Stylesheet root) {
+        StringBuilder result = new StringBuilder();
+        for (ASTNode child : root.getChildren()) {
+            if (child instanceof Stylerule) {
+                result.append(generateStylerule((Stylerule) child));
+            }
+        }
+        return result.toString();
+    }
 
-	//TODO: methode generateStylerule aanpassen met een forloop + Stringbuilder
-	// + generateStylerule mo
-	private String generateStylerule(Stylerule stylerule) {
-		String result = stylerule.selectors.get(0) + " {\n";
-		result += "\t" + generateDecleration(stylerule.body.get(0));
-		result += "\n}";
-		return result;
- 	}
+    private String generateStylerule(Stylerule stylerule) {
+        StringBuilder result = new StringBuilder();
 
-	private String generateDecleration(ASTNode astNode) {
-		return "Declaration";
-	}
+        result.append(generateSelector(stylerule));
+        result.append(" {\n");
+        result.append(generateDeclaration(stylerule));
+        result.append("}\n");
 
+        return result.toString();
+    }
 
+    private String generateSelector(Stylerule stylerule) {
+        StringBuilder result = new StringBuilder();
+        for (ASTNode selector : stylerule.selectors) {
+            result.append(selector.toString());
+        }
+        return result.toString();
+    }
+
+    private String generateDeclaration(Stylerule stylerule) {
+        StringBuilder result = new StringBuilder();
+        for (ASTNode node : stylerule.getChildren()) {
+            if (node instanceof Declaration) {
+                Declaration declaration = (Declaration) node;
+                result
+                        .append("\t ")
+                        .append(declaration.property.name)
+                        .append(": ");
+                if (declaration.expression != null) {
+                    result.append(generateExpression(declaration.expression));
+                }
+                result.append(";\n");
+            }
+        }
+        return result.toString();
+    }
+
+    private String generateExpression(Expression expression) {
+        StringBuilder exprResult = new StringBuilder();
+
+        if (expression instanceof PixelLiteral) {
+            PixelLiteral pixelLiteral = (PixelLiteral) expression;
+            exprResult
+                    .append(pixelLiteral.value)
+                    .append("px");
+        } else if (expression instanceof PercentageLiteral) {
+            PercentageLiteral percentageLiteral = (PercentageLiteral) expression;
+            exprResult
+                    .append(percentageLiteral.value)
+                    .append("%");
+        } else if (expression instanceof ColorLiteral) {
+            ColorLiteral colorLiteral = (ColorLiteral) expression;
+            exprResult.append(colorLiteral.value);
+        } else if (expression instanceof ScalarLiteral) {
+            ScalarLiteral scalarLiteral = (ScalarLiteral) expression;
+            exprResult.append(scalarLiteral.value);
+        }
+        return exprResult.toString();
+    }
 }
